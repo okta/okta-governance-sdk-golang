c872f2f5e33797f6fc12b0a5bafe99a3bf76a1cf
Pranav RK <pranav.rathinakumar@okta.com>
2026-03-17 14:15:32 +0530

    release: prepare v1.1.0 (#10)

--- governance/configuration.go
+++ governance/configuration.go
@@ -105,7 +105,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "OpenAPI-Generator/1.0.1/go",
+		UserAgent:     "OpenAPI-Generator/1.1.0/go",
 		Debug:         false,
 		Servers: ServerConfigurations{
 			{
